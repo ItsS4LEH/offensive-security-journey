9517046b31025af8d90cae1368c08956211b46d8
SALEH <ItsS4LEH@gmail.com>
2026-02-15 16:09:42 +0300

    Update index.php
    Update index.php

---
+++ S4PwnLab/TrustedID_Ex/SCF_Forging/SCF_Manip/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$message = "Welcome to my simple PHP page!";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple PHP Page</title>
+</head>
+<body>
+    <h1><?php echo $message; ?></h1>
+    <p>This page runs PHP and displays a message using a variable.</p>
+</body>
+</html>
